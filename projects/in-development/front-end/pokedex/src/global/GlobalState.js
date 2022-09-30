f7e79192ca4ded42a324f8a61e833a47578fa064
Caio Rigotto <caiorigotto@gmail.com>
2022-09-30 14:44:11 -0300

    global state and cards

--- projects/in-development/front-end/pokedex/src/global/GlobalState.js
+++ projects/in-development/front-end/pokedex/src/global/GlobalState.js
@@ -1,17 +1,53 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import BASE_URL from '../constants/BASE_URL'
 import useRequestData from "../hooks/useRequestData";
 import GlobalStateContext from "./GlobalStateContext";
 
 
 const GlobalState = (props) => {
-    const [pkmData, isLoading] = useRequestData(`${BASE_URL}pokemon/?limit=20`)
+    const [apiData, isLoading] = useRequestData(`${BASE_URL}pokemon/?limit=20`)
+    const [isLoadingGlobal, setLoading] = useState(true)
     const [pkmUrl, setUrl] = useState()
+    const [pkmData, setData] = useState()
 
-    
-    
+    useEffect(() => {
+        setLoading(true)
+        let urls = []
+        !isLoading && pkmData &&
+            apiData.results.map((pkm) => {
+                urls.push(pkm.url)
+                setUrl(urls)
+                return null
+            })
+    }, [apiData])
+
+    useEffect(() => {
+        setLoading(true)
+        let individualData = []
+        !isLoading && pkmUrl &&
+            pkmUrl.map((url) => {
+                axios
+                    .get(url)
+                    .then((res) => {
+                        individualData.push(res.data)
+                        console.log(individualData)
+                        if (individualData.length >= 19) {
+                            setLoading(false)
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+                return null
+            })
+        setData(individualData)
+    }, [pkmUrl])
+
+
     let globalData = {
-        pkmData, isLoading
+        pkmData,
+        isLoadingGlobal
     }
     return (
         <GlobalStateContext.Provider value={globalData}>
--- projects/in-development/front-end/pokedex/src/pages/home/Home.js
+++ projects/in-development/front-end/pokedex/src/pages/home/Home.js
@@ -6,37 +6,50 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid2 from '@mui/material/Unstable_Grid2';
 import GlobalStateContext from '../../global/GlobalStateContext';
 
 function PkmCard() {
     const data = useContext(GlobalStateContext)
-    console.log(data)
+
+    React.useEffect(() => {
+
+    }, [])
 
     return <>
         {
-            data.isLoading
+            data.isLoadingGlobal
                 ? <p>Loading</p>
-                : <Card sx={{ maxWidth: '25vw' }}>
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        image={data[0].pkmData.sprites.other.home.front_default}
-                        alt={data.pkmData.name}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {data.pkmData.name}
-                        </Typography>
-                        <Typography variant="h5" color="text.secondary">
-                            {data.pkmData.types[0].type.name}
-                            
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">Details</Button>
-                        <Button size="small">Add To Pokedex</Button>
-                    </CardActions>
-                </Card>
+                :
+                data.pkmData
+                    .sort(function (a, b) { return a.id - b.id })
+                    .map((pkm) => {
+                        return (
+                            <Grid2 xs={3}>
+                                <Card key={pkm.id} sx={{ maxWidth: '25vw' }}>
+                                    <CardMedia
+                                        component="img"
+                                        height="100%"
+                                        image={pkm.sprites.other.home.front_default}
+                                        alt={pkm.name}
+                                    />
+                                    <CardContent>
+                                        <Typography key={pkm.id} gutterBottom variant="h5" component="div">
+                                            {pkm.name}
+                                        </Typography>
+                                        <Typography variant="h5" color="text.secondary">
+                                            {pkm.types[0].type.name}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">Details</Button>
+                                        <Button size="small">Add To Pokedex</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid2>
+
+                        )
+                    })
         }
     </>
 }
@@ -44,6 +57,8 @@ function PkmCard() {
 
 export const Home = () => {
     return (
-        <PkmCard />
+        <Grid2 container spacing={2} disableEqualOverflow>
+            <PkmCard />
+        </Grid2>
     )
 }
