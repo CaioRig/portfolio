182576dae6529b1586b43e2ec920a59e3a800393
Caio Rigotto <caiorigotto@gmail.com>
2022-09-30 14:45:05 -0300

    ...

--- projects/in-development/front-end/pokedex/src/pages/home/Home.js
+++ projects/in-development/front-end/pokedex/src/pages/home/Home.js
@@ -47,7 +47,6 @@ function PkmCard() {
                                     </CardActions>
                                 </Card>
                             </Grid2>
-
                         )
                     })
         }
